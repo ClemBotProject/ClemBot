130a9189110e9c02025844c2dc0e8968d483167c
jaymaddencox <jaymaddencox@gmail.com>
2020-04-23 19:29:26 -0400

    ENV stuff and secrets json

---
+++ ClemBot.py
@@ -0,0 +1,8 @@
+import discord
+
+class ClemBot(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import discord
+import ClemBot
+
 def main():
-    print("hello world!")
+    clemBot = ClemBot.ClemBot()
+    clemBot.run("NzAzMDA3NzE5NDY4MDQwMjIy.XqIU_g.1TPNIhtRjO-GJvTgNEenSiSzLDM")
 
 if __name__ == "__main__":
     main()
