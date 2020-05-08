e025513f9e13815d6a371cacea9ffb73de54053c
jaymaddencox <jaymaddencox@gmail.com>
2020-05-08 13:28:03 -0400

    messaging framework init commit

--- bot/services/message_handling.py
+++ bot/services/message_handling.py
@@ -2,11 +2,12 @@
 from bot.events import Events
 from bot.services.base_service import BaseService
 from bot.data.message_repository import MessageRepository
+import bot.messaging.messenger as messenger
 
 class MessageHandling(BaseService):
 
     def __init__(self):
-        pass
+        messenger.subscribe(Events.on_message_recieved, self.on_message_recieved)
 
     async def on_message_recieved(self, message) -> None:
         await MessageRepository().add_message(message)
