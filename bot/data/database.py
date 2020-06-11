1a281c3801b67b82491302622501e32bbb4891c8
jaymaddencox <jaymaddencox@gmail.com>
2020-06-11 15:23:45 -0400

    Create db folder if it doenst exist

--- bot/data/database.py
+++ bot/data/database.py
@@ -1,6 +1,7 @@
-import aiosqlite
+import os
 from typing import Iterator
 
+import aiosqlite
 
 class Database:
 
@@ -8,6 +9,8 @@ class Database:
         self.database_name = name
 
     async def create_database(self) -> bool:
+        if not os.path.exists('database'):
+            os.makedirs('database')
         async with aiosqlite.connect(f'database/{self.database_name}') as db:
             with open('bot/data/CreateTables.sql') as f:
                 await db.executescript(f.read())
