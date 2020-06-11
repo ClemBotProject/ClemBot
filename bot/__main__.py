7a1103ad7e3405158ac49b13727410f2b006f5bf
jaymaddencox <jaymaddencox@gmail.com>
2020-06-11 17:22:51 -0400

    Changed Prefix

--- bot/__main__.py
+++ bot/__main__.py
@@ -54,7 +54,7 @@ def main():
         BotSecrets.get_instance().bot_token = token
 
     bot_log.info('Bot Starting Up')
-    ClemBot(command_prefix = ':').run(BotSecrets.get_instance().bot_token)
+    ClemBot(command_prefix = '$').run(BotSecrets.get_instance().bot_token)
 
 
 if __name__ == "__main__":
