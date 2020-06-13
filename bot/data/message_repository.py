7d43406c24c976cf2445b2dcc5a624726d0b9bb6
jaymaddencox <jaymaddencox@gmail.com>
2020-06-12 21:24:20 -0400

    DB layer key error fix

--- bot/data/message_repository.py
+++ bot/data/message_repository.py
@@ -6,6 +6,9 @@ from bot.data.base_repository import BaseRepository
 class MessageRepository(BaseRepository):
 
     async def add_message(self, message):
+        if await self.check_message(message.id):
+            return
+
         async with aiosqlite.connect(self.resolved_db_path) as db:
             await db.execute(
                 """
@@ -16,3 +19,7 @@ class MessageRepository(BaseRepository):
                 """, (message.id, message.guild.id, message.channel.id, message.author.id, message.content))
             await db.commit()
 
+    async def check_message(self, message: int) -> bool:
+        async with aiosqlite.connect(self.resolved_db_path) as db:
+            async with db.execute('SELECT * FROM Messages WHERE id = ?', (guild_id,)) as c:
+                return await c.fetchone() is not None
--- bot/data/user_repository.py
+++ bot/data/user_repository.py
@@ -8,10 +8,14 @@ log = logging.getLogger(__name__)
 class UserRepository(BaseRepository):
 
     async def add_user(self, user: str, guild_id: int) -> None:
-        try:
-            async with aiosqlite.connect(self.resolved_db_path) as db:
-                await db.execute('INSERT INTO Users (id, fk_guildId, name) VALUES (?, ?, ?)', (user.id, guild_id, user.name))
-                await db.execute('INSERT INTO Users_Guilds VALUES (?, ?)', (guild_id, user.id))
-                await db.commit()
-        except sqlite3.IntegrityError as e:
-            raise PrimaryKeyError(f'Failed to insert user: {e}')
+        if await self.check_user(user.id):
+            return
+
+        async with aiosqlite.connect(self.resolved_db_path) as db:
+            await db.execute('INSERT INTO Users (id, fk_guildId, name) VALUES (?, ?, ?)', (user.id, guild_id, user.name))
+            await db.execute('INSERT INTO Users_Guilds VALUES (?, ?)', (guild_id, user.id))
+
+    async def check_user(self, user_id: int) -> bool:
+        async with aiosqlite.connect(self.resolved_db_path) as db:
+            async with db.execute('SELECT * FROM Users WHERE id = ?', (user_id,)) as c:
+                return await c.fetchone() is not None
