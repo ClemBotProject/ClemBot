49cd1fec3578b3ebf02e750ef895bf29b6dee731
Nathan-Binkley <nbink2@gmail.com>
2020-08-14 22:10:03 -0400

    Added only grades, not memes

--- bot/cogs/grades_cog.py
+++ bot/cogs/grades_cog.py
@@ -12,21 +12,16 @@ from bot.consts import Colors
 
 log = logging.getLogger(__name__)
 
-
-
 class gradesCog(commands.Cog):
     special_converted_files = ['2018Fall.csv','2018Spring.csv'] #these files have special formatting for names (ie ' "Smith, John William" ' is normal and in these it's simply 'Smith John William' without commas or quotes )
     #TBH, this is the easier way to parse but whatevs
     converted_files = ['2013Fall.csv', '2014Spring.csv', '2014Fall.csv', '2015Spring.csv', '2015Fall.csv', '2016Spring.csv', '2016Fall.csv', '2017Spring.csv', '2017Fall.csv', '2019Spring.csv', '2019Fall.csv', '2020Spring.csv']
-
 
     def __init__(self, bot):
         self.bot = bot
         self.fileLines = []
         self.master_list = {}
         self.master_prof_list = {}
-
-
 
     def process_Search(self, orig_query: str) -> str: #Primary search function and processing for a query. A query is generally defined by 
                                     #"Course-Number" of which it pulls the data from the DB.
@@ -36,27 +31,6 @@ class gradesCog(commands.Cog):
         items = query.split("-")
         master_String = ''
 
-        # if("ALL" in items):
-        #     print("ALL IN QUERY")
-        #     print("HERE COMES THE SUN")
-        #     if items[0] == "ALL" and items[1] == "ALL":
-        #         print("ALLALL")
-        #         for i in master_list:
-        #             master_String += process_Search(i)
-        #     elif items[0] == "ALL":
-        #         for i in master_list:
-        #             if items[1] in i:
-        #                 master_String += process_Search(i)
-        #     elif items[1] == "ALL":
-        #         for i in master_list:
-        #             if items[0] in i:
-        #                 master_String += process_Search(i)
-        #     else:
-        #         print("Misunderstood")
-        #         return "HELP"
-        #     return master_String
-
-        # else:
         if query in self.master_list:
             data_list = self.master_list[query]
         else:
@@ -99,7 +73,6 @@ class gradesCog(commands.Cog):
 
         courseString = "Average; A: {}%\nB: {}%\nC: {}%\nD: {}%\nF: {}%\nW: {}%\nfrom {} class(es) for {}: {}".format(AvgA, AvgB, AvgC, AvgD, AvgF, AvgWithdraw, len(data_list), orig_query, name)
         
-
         bestProfName = "" #Professor name to go in professor string
         bestProfAB = 0
         bestProfLenCount = 0
@@ -204,39 +177,17 @@ class gradesCog(commands.Cog):
             items[i] = items[i][0].upper() + items[i][1:].lower()
         return " ".join(items)
 
-    def getAllCourses(self):
-        temp = []
-        for i in self.master_list:
-            if i[:-5] not in temp:
-                temp.append(i[:-5])
-            else:
-                pass
-        with open("bot/cogs/Student-Teacher/assets/CourseList.txt", "w") as f:
-            for i in range(len(temp)):
-                f.write(temp[i]+"\n")
-
     def searchCourse(self, query):
-        # query = input("What course do you want to search? (Format: <Course>-<number> ie CPSC-3720)\n")
         string = ""
-        # try:
-            
-        # string += "\n--------------------------------------------------------\n"
-        string += "Since Spring 2014, there was an average of " + self.process_Search(query)
-        string += "DISCLAIMER:\n"
-        string += "Not every professor listed will be at Clemson, this is a tool built for better information but not complete information\n"
-        # string += "Take it at your own discression\n"
-        # string += "\nIn addition, this system works on the Grade Distribution Releases located at https://www.clemson.edu/institutional-effectiveness/oir/data-reports/\n"
-        # string += "As a result, the limitations according to the GDR are as follows:\n"
+        string += 'Since Spring 2014, there was an average of ' + self.process_Search(query)
+        string += 'DISCLAIMER:\n'
+        string += 'Not every professor listed will be at Clemson, this is a tool built for better information but not complete information\n'
+        string += "Take it at your own discression\n"
+        string += "\nIn addition, this system works on the Grade Distribution Releases located at https://www.clemson.edu/institutional-effectiveness/oir/data-reports/\n"
+        string += "As a result, the limitations according to the GDR are as follows:\n"
         string += '*Course Sections that meet the following conditions are not included: Undergraduate classes with less than 10 students or Graduate classes with less than 5 students. In addition, if a section has all but 1 student making a common grade (example: All but one student makes a "B" in a class), the section is excluded.*'
-        # string += '\n----------------------------------------------------------------------------'
         return string
-        # except NotADirectoryError as e:
-        #     print("Class not found, are you sure you used the correct format? (Ex: cpsc-2120)")
         
-        # except Exception as e:
-        #     print("Something went wrong somewhere, idk bout that")
-        #     print(e)
-
     def go(self, course):
         self.initialize()
         self.getAllCourses()
