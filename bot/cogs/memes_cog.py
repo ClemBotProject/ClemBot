cb627462ebec59730f5abf65a5f49b52191e3837
Nathan-Binkley <nbink2@gmail.com>
2020-08-19 00:31:05 -0400

    added spongebob command

--- bot/cogs/memes_cog.py
+++ bot/cogs/memes_cog.py
@@ -3,6 +3,12 @@ import logging
 import discord
 import discord.ext.commands as commands
 
+from random import randint
+from random import seed
+from time import time
+
+from bot.consts import Colors
+
 log = logging.getLogger(__name__)
 
 class MemesCog(commands.Cog):
@@ -20,6 +26,40 @@ class MemesCog(commands.Cog):
             msg += '\n'
 
         await ctx.send(msg)
+
+    @commands.command()
+    async def spongebob(self, ctx, *, args):
+
+        '''
+        Spongebob Text
+        '''
+        seed(time())
+        args = args.replace('"', "'")
+    
+        result = ''
+        for i in args:
+            helper = randint(0, 100)
+            
+            if helper > 60:
+                result += str(i).upper()
+            else:
+                result += str(i).lower()
+
+        try:
+            embed = discord.Embed(title="SpOnGeBoB", color=Colors.ClemsonOrange)
+            result2 = ''
+            if len(result) >= 1024:
+                result2 = result[1024:len(result)]
+                result = result[:1023]
+            embed.add_field(name="TeXt", value=result, inline=False)
+            if result2:
+                print("RESULT2\n\n\n")
+                embed.add_field(name="tExT", value=result2, inline=False)
+        except Exception as e:
+            embed = discord.Embed(title="SpOnGeBoB", color=Colors.Error)
+            embed.add_field(name="ERROR", value=e, inline=False)
+
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(MemesCog(bot))
