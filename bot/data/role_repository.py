2110fc250c0334e69ed093febd7d4c0647960e8c
Jay <jaymaddencox@gmail.com>
2020-08-25 17:35:59 -0400

    Fixed roles becoming unassignable if they are edited

--- bot/data/role_repository.py
+++ bot/data/role_repository.py
@@ -9,15 +9,15 @@ log = logging.getLogger(__name__)
 
 class RoleRepository(BaseRepository):
 
-    async def add_or_update_role(self, role: str, guild_id: int, *, is_assignable: bool = False) -> None:
+    async def add_or_update_role(self, role: str, guild_id: int) -> None:
         async with aiosqlite.connect(self.resolved_db_path) as db:
             await db.execute(
                 """
-                INSERT INTO Roles (id, fk_guildId, name, position, isRoleAssignable) VALUES (?, ?, ?, ?, ?)
+                INSERT INTO Roles (id, fk_guildId, name, position) VALUES (?, ?, ?, ?)
                 ON CONFLICT(id) DO UPDATE SET
                     name = excluded.name,
                     position = excluded.position
-                """, (role.id, guild_id, role.name, role.position, is_assignable))
+                """, (role.id, guild_id, role.name, role.position))
             await db.commit()
 
     async def delete_role(self, role_id: int) -> None:
