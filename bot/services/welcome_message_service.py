4e47f570dde9c760b6432e93118486726502be28
Jay Madden <jaymaddencox@gmail.com>
2020-08-27 00:44:12 -0400

    Added the ability to define a welcome to the server dm (#163)

---
+++ bot/services/welcome_message_service.py
@@ -0,0 +1,25 @@
+import logging
+
+from bot.services.base_service import BaseService
+from bot.messaging.events import Events
+from bot.data.welcome_message_repository import WelcomeMessageRepository
+
+log = logging.getLogger(__name__)
+
+
+class WelcomeMessageService(BaseService):
+
+    def __init__(self, *, bot):
+        super().__init__(bot)
+    
+    @BaseService.Listener(Events.on_user_joined)
+    async def assignable_role_add(self, user):
+        repo = WelcomeMessageRepository()
+
+        message = await repo.get_welcome_message(user.guild.id)
+
+        if message and not user.bot:
+            await user.send(message)
+
+    async def load_service(self):
+        pass
