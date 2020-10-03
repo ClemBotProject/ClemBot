c0796c747b8549cf75f3d3330017fadf20fb1af0
MasterHawk <MacsterHawk@users.noreply.github.com>
2020-10-03 12:08:20 -0400

    Revert "'Vastly simplified the previous base_converter_cog.py code that I had before'"
    This reverts commit 6c19139f6fb6e5cd154cc96a59ca120d42993e97.

--- bot/cogs/base_converter_cog.py
+++ bot/cogs/base_converter_cog.py
@@ -5,60 +5,127 @@ from bot.consts import Colors
 
 class BaseConverterCog(commands.Cog):
 
-    def __init__(self, bot) -> None:
+    def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(aliases= ['convert','baseconvert'])
-    async def bconvert(self, ctx, *args):
+    #if number less than <
+
+    @commands.group()
+    async def baseconvert(self, ctx):
         """
         A simple base converter that takes in a base number an a value, then displays the value in binary, octal, decimal, and hexadecimal
-        
-            Examples:
-            bconvert [bin / binary] [11 / 0b11]
-            bconvert [dec / decimal] [99]
-            bconvert [hex / hexadecimal] [FF / 0xFF]
-            bconvert [oct / octal] [77 / 0o77]
+        Examples:
+            baseconvert(bin 0b11010101)
+            baseconvert(oct 0o1234)
+            baseconvert(dec 4567)
+            baseconvert(hex 0xAF13)
         """
-        base = args[0]
-        number = args[1]
-        if base is None or number is None:
-            await ctx.send('Missing arguments! Use the help command for examples')
-            return
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Invalid Subcommand!')
 
-        if base == 'bin' or base == 'binary':
-            number = int(number, 2)
-
-        elif base == 'dec' or base == 'decimal':
-            number = int(number)
-
-        elif base == 'hex' or base == 'hexadecimal':
-            number = int(number, 16)
-
-        elif base == 'oct' or base == 'octal':
-            number = int(number, 8)
-
-        else:
-            await ctx.send('Invalid Entry! Use the help command for examples')
-            return
-
-        b = bin(number)
+    @baseconvert.command()
+    async def bin(self, ctx, number : str):
+        number = int(number, 2)
         d = int(number)
-        h = hex(number)
         o = oct(number)	
-        if number < 1114112: 
+        h = hex(number)
+        if number in range(1114112): 
             a = chr(number)
         else:
             a = 'Unicode representation not found.'
 
         embed = discord.Embed(title='Conversions', description =f'Numerical Conversions of {number}', color = Colors.ClemsonOrange)
-        embed.set_image(url = 'https://i.gifer.com/8oXf.gif')
-        embed.add_field(name='Binary', value=b, inline=False)
         embed.add_field(name='Decimal', value=d, inline=False)
         embed.add_field(name='Hexadecimal', value=h, inline=False)
         embed.add_field(name='Octal', value=o, inline=False)
         embed.add_field(name="UTF-8", value=a, inline=False)
         
         await ctx.send(embed=embed)
+        
+    @bin.error
+    async def bin_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('Input Binary is Invalid')
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('Input Binary is Invalid')
+
+    @baseconvert.command()
+    async def dec(self, ctx, number : int):
+        b = bin(number)
+        h = hex(number)
+        o = oct(number)
+        if number in range(1114112):
+            a = chr(number)
+        else:
+            a = 'Unicode representation not found.'
+
+        embed = discord.Embed(title='Conversions', description =f'Numerical Conversions of {number}', color = Colors.ClemsonOrange)
+        embed.add_field(name='Binary', value=b, inline=False)
+        embed.add_field(name='Hexadecimal', value=h, inline=False)
+        embed.add_field(name='Octal', value=o, inline=False)
+        embed.add_field(name='UTF-8', value=a, inline=False)
+        
+        await ctx.send(embed=embed)
+
+    @dec.error
+    async def dec_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('Input Decimal is Invalid')
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('Input Decimal is Invalid')
+
+    @baseconvert.command()
+    async def hex(self, ctx, number : str):
+        number = int(number, 16)
+        b = bin(number)
+        d = int(number)
+        o = oct(number)
+
+        if number in range(1114112):
+            a = chr(number)
+        else:
+            a = 'Unicode representation not found.'
+
+        embed = discord.Embed(title='Conversions', description =f'Numerical Conversions of {number}', color = Colors.ClemsonOrange)
+        embed.add_field(name='Binary', value=b, inline=False)
+        embed.add_field(name='Decimal', value=d, inline=False)
+        embed.add_field(name='Octal', value=o, inline=False)
+        embed.add_field(name='UTF-8', value=a, inline=False)
+        
+        await ctx.send(embed=embed)
+
+    @hex.error
+    async def hex_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('Input Hexadecimal is Invalid')
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('Input Hexadecimal is Invalid')
+
+    @baseconvert.command()
+    async def oct(self, ctx, number : str):
+        number = int(number, 8)
+        b = bin(number)
+        d = int(number)
+        h = hex(number)
+        if number in range(1114112): 
+            a = chr(number)
+        else:
+            a = 'Unicode representation not found.'
+
+        embed = discord.Embed(title='Conversions', description =f'Numerical Conversions of {number}', color = Colors.ClemsonOrange)
+        embed.add_field(name='Binary', value=b, inline=False)
+        embed.add_field(name='Decimal', value=d, inline=False)
+        embed.add_field(name='Hexadecimal', value=h, inline=False)
+        embed.add_field(name='UTF-8', value=a, inline=False)
+        
+        await ctx.send(embed=embed)
+
+    @oct.error
+    async def oct_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('Input Octal is Invalid')
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('Input Octal is Invalid')
 
 def setup(bot):
     bot.add_cog(BaseConverterCog(bot))
