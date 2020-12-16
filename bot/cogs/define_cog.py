c898b0274d4ce34c14ca4aa5dbad681f90cdd05a
Rajat Sethi <rajatsethi2001@gmail.com>
2020-12-15 19:56:28 -0500

    Fixed errors in define_cog

--- bot/cogs/define_cog.py
+++ bot/cogs/define_cog.py
@@ -7,10 +7,10 @@ import discord
 import discord.ext.commands as commands
 from bot.messaging.events import Events
 from bot.bot_secrets import BotSecrets
+from bot.consts import Colors
 
 import json
-import requests
-
+import aiohttp
 
 log = logging.getLogger(__name__)
 
@@ -21,59 +21,36 @@ class defineCog(commands.Cog):
         self.api_key = BotSecrets.get_instance().merriam_key
         self.wordPages = []
     
-    def getPageData(self, response, word):
-        jsonData = response.json()
+    def getPageData(self, jsonData, word):
         pages = []
 
         #If the word is found, the JSON will return a dictionary of information. 
         if (isinstance(jsonData[0], dict)):
 
             #For words with several definitions, it will return several dictionaries. 
-            for i in range(len(jsonData)):
-
-                #Current dictionary of information.
-                wordData = jsonData[i]
-
+            for wordData in jsonData:
                 #Stems of the given word (Past Tense, Future Tense, Perfect Tense, etc.)
-                wordStems = []
-                try:
-                    wordStems = wordData['meta']['stems']
-                except:
-                    pass
+                wordStems = wordData.get('meta',{}).get('stems',[])
                 
                 #Syllables of the given word
-                syllableData = ""
-                try:
-                    syllableData = wordData['hwi']['hw']
-                except:
-                    pass
+                syllableData = wordData.get('hwi',{}).get('hw',"")
                 
                 #Pronunciation of the given word (With those weird letters)
                 pronunc = []
-                try:
-                    prsData = wordData['hwi']['prs']
-                    for prs in range(len(prsData)):
-                        soundData = prsData[i]
-                        pronunc.append(soundData['mw'])
-
-                except:
-                    pass
+                prsData = wordData.get('hwi',{}).get('prs',[])
+                
+                for soundData in prsData:
+                    pronunc.append(soundData.get('mw',""))
                 
                 #Type of the given word (Noun, Verb, Adjective, etc.)
-                wordType = ""
-                try:
-                    wordType = wordData['fl']
-                except:
-                    pass
+                wordType = wordData.get('fl',"")
                 
                 #Definitions of the given word
                 definitions = []
-                try:
-                    defData = wordData['shortdef']
-                    for defin in range(len(defData)):
-                        definitions.append(defData[defin])
-                except:
-                    definitions.append("Definition Missing")
+                defData = wordData.get('shortdef',[])
+                
+                for defin in defData:
+                    definitions.append(defin)
                 
                 #Turn data into one long string (represents a page)
                 template = "Tenses: "
@@ -117,31 +94,34 @@ class defineCog(commands.Cog):
         """
         Given a word, find its definition and any other relevant information
         
-        USE: !define <word>
-        EXAMPLE: !define schadenfreude
+        USE: define <word>
+        EXAMPLE: define schadenfreude
 
         For phrases, use underscores
-        EXAMPLE: !define computer_science
+        EXAMPLE: define computer_science
         """
 
         actualWord = word.replace("_"," ")
-        word = word.replace("_","%20")
-        url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word.lower() + "?key=" + self.api_key
-        response = requests.get(url)
-        
-        if (response.status_code == 200):
-            self.wordPages = self.getPageData(response, word)
+        word = word.replace("_","%20").lower()
+        url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={self.api_key}'
+        async with aiohttp.request('get', url) as response:
+            if (response.status == 200):
+                jsonData = await response.json()
+                self.wordPages = self.getPageData(jsonData, word)
 
-        else:
-            ErrMsg = "Oh No! There appears to be an issue! Yell at one of the developers with the following code.\nError Code: " + str(response.status_code)
-            self.wordPages = [ErrMsg]
-        
-        await self.bot.messenger.publish(Events.on_set_pageable,
-            embed_name = "Merriam-Webster Dictionary",
-            field_title = f'Word: {actualWord}',
-            pages = self.wordPages,
-            author = ctx.author,
-            channel = ctx.channel)
+            else:
+                embed = discord.Embed(title="Merriam_Webster Dictionary", color=Colors.Error)
+                ErrMsg = f'Oh No! There appears to be an issue! Yell at one of the developers with the following code.\nError Code: {str(response.status)}'
+                embed.add_field(name="Error with API", value=ErrMsg, inline=False)
+                await ctx.send(embed=embed)
+                return
+                    
+            await self.bot.messenger.publish(Events.on_set_pageable,
+                embed_name = "Merriam-Webster Dictionary",
+                field_title = f'Word: {actualWord}',
+                pages = self.wordPages,
+                author = ctx.author,
+                channel = ctx.channel)
     
 def setup(bot):
     bot.add_cog(defineCog(bot))
