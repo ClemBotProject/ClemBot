946f32172f81b9ff1e72d2d3afb38b626cf06fb4
Rajat Sethi <rajatsethi2001@gmail.com>
2020-12-16 01:48:47 -0500

    Small details added

--- bot/cogs/define_cog.py
+++ bot/cogs/define_cog.py
@@ -13,13 +13,13 @@ import json
 import aiohttp
 
 log = logging.getLogger(__name__)
+API_URL = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
 
 class defineCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         self.api_key = BotSecrets.get_instance().merriam_key
-        self.wordPages = []
     
     def getPageData(self, jsonData, word):
         pages = []
@@ -33,17 +33,17 @@ class defineCog(commands.Cog):
                 wordStems = wordData.get('meta',{}).get('stems',[])
                 
                 #Syllables of the given word
-                syllableData = wordData.get('hwi',{}).get('hw',"")
+                syllableData = wordData.get('hwi',{}).get('hw','')
                 
                 #Pronunciation of the given word (With those weird letters)
                 pronunc = []
                 prsData = wordData.get('hwi',{}).get('prs',[])
                 
                 for soundData in prsData:
-                    pronunc.append(soundData.get('mw',""))
+                    pronunc.append(soundData.get('mw',''))
                 
                 #Type of the given word (Noun, Verb, Adjective, etc.)
-                wordType = wordData.get('fl',"")
+                wordType = wordData.get('fl','')
                 
                 #Definitions of the given word
                 definitions = []
@@ -53,37 +53,37 @@ class defineCog(commands.Cog):
                     definitions.append(defin)
                 
                 #Turn data into one long string (represents a page)
-                template = "Tenses: "
-                for s in range(len(wordStems)):
-                    template += wordStems[s]
-                    if (s != len(wordStems) - 1):
-                        template += ", "
-                template += "\n"
+                template = 'Tenses: '
+                for s in enumerate(wordStems):
+                    template += s[1]
+                    if s[0] != len(wordStems) - 1:
+                        template += ', '
+                template += '\n'
 
-                template += "Syllables: " + syllableData + "\n"
+                template += f'Syllables: {syllableData}\n'
 
-                template += "Pronunciation: "
-                for s in range(len(pronunc)):
-                    template += pronunc[s]
-                    if (s != len(pronunc) - 1):
-                        template += ", "
-                template += "\n"
+                template += 'Pronunciation: '
+                for s in enumerate(pronunc):
+                    template += s[1]
+                    if s[0] != len(pronunc) - 1:
+                        template += ', '
+                template += '\n'
 
-                template += "Word Type: " + wordType + "\n"
+                template += f'Word Type: {wordType}\n'
 
-                template += "\n"
-                for s in range(len(definitions)):
-                    page = template + "Definition: " + definitions[s]
-                    page = page.replace("*"," | ")
+                template += '\n'
+                for s in enumerate(definitions):
+                    page = f'{template}Definition: {s[1]}'
+                    page = page.replace('*',' | ')
                     pages.append(page)
 
         #If the word cannot be found, the JSON returns a list of other possible suggestions. 
-        elif (isinstance(jsonData[0], str)):
-            template = "Word not found, see also: "
-            for s in range(len(jsonData)):
-                template += jsonData[s]
-                if (s != len(jsonData) - 1):
-                    template += ", "
+        elif isinstance(jsonData[0], str):
+            template = f'Word not found, see also: '
+            for s in enumerate(jsonData):
+                template = f'{template} {s[1]}'
+                if s[0] != len(jsonData) - 1:
+                    template = f'{template}, '
     
             pages = [template]
 
@@ -101,25 +101,26 @@ class defineCog(commands.Cog):
         EXAMPLE: define computer_science
         """
 
-        actualWord = word.replace("_"," ")
-        word = word.replace("_","%20").lower()
-        url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={self.api_key}'
+        actualWord = word.replace('_',' ')
+        word = word.replace('_','%20').lower()
+        url = f'{API_URL}{word}?key={self.api_key}'
+        wordPages = []
         async with aiohttp.request('get', url) as response:
-            if (response.status == 200):
+            if response.status == 200:
                 jsonData = await response.json()
-                self.wordPages = self.getPageData(jsonData, word)
+                wordPages = self.getPageData(jsonData, word)
 
             else:
-                embed = discord.Embed(title="Merriam_Webster Dictionary", color=Colors.Error)
-                ErrMsg = f'Oh No! There appears to be an issue! Yell at one of the developers with the following code.\nError Code: {str(response.status)}'
-                embed.add_field(name="Error with API", value=ErrMsg, inline=False)
+                embed = discord.Embed(title='Merriam_Webster Dictionary', color=Colors.Error)
+                ErrMsg = f'Oh No! There appears to be an issue! Yell at one of the developers with the following code.\nError Code: {response.status}'
+                embed.add_field(name='Error with API', value=ErrMsg, inline=False)
                 await ctx.send(embed=embed)
                 return
                     
             await self.bot.messenger.publish(Events.on_set_pageable,
-                embed_name = "Merriam-Webster Dictionary",
+                embed_name = 'Merriam-Webster Dictionary',
                 field_title = f'Word: {actualWord}',
-                pages = self.wordPages,
+                pages = wordPages,
                 author = ctx.author,
                 channel = ctx.channel)
     
