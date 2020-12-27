04ff9eccdddad66742f1f929e78f2fef38d51c8c
Jay Madden <jaymaddencox@gmail.com>
2020-12-27 16:36:28 -0500

    Added guild status to define if a bot is currently in the guild (#245)

--- bot/services/guild_handling_service.py
+++ bot/services/guild_handling_service.py
@@ -15,6 +15,7 @@ class GuildHandlingService(BaseService):
     @BaseService.Listener(Events.on_guild_joined)
     async def on_guild_joined(self, guild) -> None:
         log.info(f'Loading guild {guild.name}: {guild.id}')
+
         await GuildRepository().add_guild(guild)
 
         #The guild has been initialized, broadcast this to the rest
@@ -22,10 +23,24 @@ class GuildHandlingService(BaseService):
         await self.bot.messenger.publish(Events.on_new_guild_initialized, guild)
         log.info(f'Guild {guild.name}: {guild.id} loaded')
 
+    @BaseService.Listener(Events.on_guild_leave)
+    async def on_guild_leave(self, guild) -> None:
+        log.info(f'Bot removed from {guild.name}: {guild.id}')
+        await GuildRepository().set_guild_status(guild.id, False)
+
     async def add_guild(self, guild) -> None:
         await GuildRepository().add_guild(guild)
 
     async def load_service(self):
+        guild_repo = GuildRepository()
+
+        db_guilds =  await guild_repo.get_all_guilds_ids()
+        api_guilds = [r.id for r in self.bot.guilds]
+
+        for removed_id in set(db_guilds) - set(api_guilds):
+            log.info(f'Guild {removed_id} removed from active servers')
+            await guild_repo.set_guild_status(removed_id, False)
+
         for guild in self.bot.guilds:
             log.info(f'Loading guild {guild.name}: {guild.id}')
             await self.add_guild(guild)
