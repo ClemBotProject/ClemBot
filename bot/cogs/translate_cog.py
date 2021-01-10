7b999d1a9d5ff9f2ac365cc50538cd945c9afda0
smathep <patricksmathers@icloud.com>
2021-01-10 01:46:20 -0500

    Implemented requested changes

--- bot/cogs/translate_cog.py
+++ bot/cogs/translate_cog.py
@@ -6,6 +6,7 @@ import uuid
 import bot.extensions as ext
 import discord.ext.commands as commands
 from discord.ext.commands.errors import UserInputError
+from bot.messaging.events import Events
 from bot.bot_secrets import BotSecrets
 from bot.consts import Colors
 
@@ -16,8 +17,8 @@ language_name_to_short_code = {
     "arabic": "ar",
     "bulgarian": "bg",
     "catalan": "ca",
-    "chinese simplified": "zh-Hans",
-    "chinese traditional":	"zh-Hant",
+    "chinese simplified": "zh-hans",
+    "chinese traditional":	"zh-hant",
     "croatian": "hr",
     "czech": "cs",
     "danish": "da",
@@ -40,7 +41,7 @@ language_name_to_short_code = {
     "japanese": "ja",
     "klingon": "tlh-Latn",
     "korean": "ko",
-    "kurdish (central)": "ku-Arab",
+    "kurdish (central)": "ku-arab",
     "latvian": "lv",
     "lithuanian": "lt",
     "malay": "ms",
@@ -52,8 +53,8 @@ language_name_to_short_code = {
     "portuguese": "pt",
     "romanian": "ro",
     "russian": "ru",
-    "serbian (cyrillic)": "sr-Cyrl",
-    "serbian (latin)": "sr-Latn",
+    "serbian (cyrillic)": "sr-cyrl",
+    "serbian (latin)": "sr-catn",
     "slovak": "sk",
     "slovenian": "sl",
     "spanish": "es",
@@ -68,8 +69,8 @@ language_name_to_short_code = {
     "welsh": "cy",
     "yucatec maya": "yua"
 }
-
-language_short_code_to_name = {value : key for (key, value) in language_name_to_short_code.items()}
+chunk_size = 15
+language_short_code_to_name = {value : key for key, value in language_name_to_short_code.items()}
 
 headers = {
     'Ocp-Apim-Subscription-Key': BotSecrets.get_instance().translator_subscription_key,
@@ -85,7 +86,6 @@ class TranslateCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self._last_member = None
 
     @ext.command()
     @ext.long_help('Allows you to translate words or sentences by either specifying both the input and output language with the text to translate, or just the output language and the text to translate')
@@ -95,26 +95,27 @@ class TranslateCog(commands.Cog):
         if len(input) < 2:
             raise UserInputError("Incorrect Number of Arguments. Minimum of 2 arguments")
         if is_valid_lang_code(input[1]):
-            output_lang = get_lang_code(input[1])
             await self.translate_given_lang(ctx, input)
         else:
             await self.translate_detect_lang(ctx, input)
     
     async def translate_given_lang(self, ctx, input):
-        input_lang = input[0]
-        output_lang = input[1]
-        text = ''
-        for i in input[2:]:
-            text += f'{i} '
+        input_lang = await get_lang_code(self, ctx, input[0])
+        output_lang = await get_lang_code(self, ctx, input[1])
+
+        if input_lang == None or output_lang == None:
+            return
+
+        text = ''.join(input[2:])
 
         params = {}
 
-        log.info('Input Lang Code: ' + str(get_lang_code(input_lang)))
-        log.info('Output Lang Code: ' + str(get_lang_code(output_lang)))
+        log.info(f'Input Lang Code: {input_lang}')
+        log.info(f'Output Lang Code: {output_lang}')
         params = {
             'api-version': '3.0',
-            'from': get_lang_code(input_lang),
-            'to': get_lang_code(output_lang)
+            'from': input_lang,
+            'to': output_lang
         }
        
         body = [{
@@ -130,19 +131,21 @@ class TranslateCog(commands.Cog):
         name = 'Translated to ' + language_short_code_to_name[response[0]['translations'][0]['to']]
         embed.add_field(name=name, value = response[0]['translations'][0]['text'], inline=False)
         await ctx.send(embed=embed)
-        return
         
     async def translate_detect_lang(self, ctx, input):
-        output_lang = input[0]
+        output_lang = await get_lang_code(self, ctx, input[0])
+        if output_lang == None: 
+            return
+
         text = ''
         for i in input[1:]:
             text += f'{i} '
-
-        log.info('Output Lang Code: ' + str(get_lang_code(output_lang)))
+        output_lang = await get_lang_code(self, ctx, output_lang)
+        log.info(f'Output Lang Code: {output_lang}')
         
         params = {
             'api-version': '3.0',
-            'to': get_lang_code(output_lang)
+            'to': output_lang
         }
         
         body = [{
@@ -151,7 +154,7 @@ class TranslateCog(commands.Cog):
         
         async with aiohttp.ClientSession() as session:
             async with await session.post(url = TRANSLATE_API_URL, params = params, headers = headers, json = body) as resp: 
-                    response = json.loads(await resp.text())
+                response = json.loads(await resp.text())
         
         log.info(response[0]['detectedLanguage'])
         log.info(response[0]['translations'])
@@ -166,14 +169,29 @@ class TranslateCog(commands.Cog):
 def is_valid_lang_code(input: str):
     return input.lower() in language_short_code_to_name or input.lower() in language_name_to_short_code
 
-def get_lang_code(input: str):
+async def get_lang_code(self, ctx, input: str):
     if input.lower() in language_short_code_to_name:
         return input.lower()
     else: 
         try:
             return language_name_to_short_code[input.lower()]
         except KeyError:
-            raise UserInputError('Given language \'' + input + '\' not valid')
+            pages = get_language_list()
+            await self.bot.messenger.publish(Events.on_set_pageable_text,
+                embed_name='Languages',
+                field_title='Given language \'' + input + '\' not valid. Here are the available languages:',
+                pages = pages,
+                author=ctx.author,
+                channel=ctx.channel)
+
+def get_language_list():
+    languages_dict = []
+    for i in range(0, len(language_name_to_short_code)):
+        languages_dict.append(list(language_name_to_short_code)[i] + ' (' + language_name_to_short_code[list(language_name_to_short_code)[i]] + ')')
+    pages = []
+    for i in range(0, len(languages_dict), chunk_size):
+        pages.append('\n'.join(languages_dict[i:i+chunk_size]))
+    return pages
 
 def setup(bot): 
     bot.add_cog(TranslateCog(bot))
