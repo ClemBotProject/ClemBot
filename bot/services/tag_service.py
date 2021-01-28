cba00fdd5492a1b705acc4febd69ac8345a3b552
Exper1mental <Exper1mental@users.noreply.github.com>
2021-01-28 11:02:43 -0500

    Updated on_message_received events
    Replaced remaining instrances of `on_message_received` with `on_guild_message_received`

--- bot/services/tag_service.py
+++ bot/services/tag_service.py
@@ -19,8 +19,8 @@ class TagService(BaseService):
     def __init__(self, *, bot):
         super().__init__(bot)
     
-    @BaseService.Listener(Events.on_message_received)
-    async def on_message_received(self, message: discord.Message) -> None:
+    @BaseService.Listener(Events.on_guild_message_received)
+    async def on_guild_message_received(self, message: discord.Message) -> None:
         if not isinstance(message.guild, discord.guild.Guild):
             return
         
