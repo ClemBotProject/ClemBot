e287d5e374fbe270c058034e16ba24c197f61881
Jay <jaymaddencox@gmail.com>
2021-04-26 10:21:38 -0400

    Added more claims to infraction view

--- bot/cogs/moderation_cog/infractions_cog.py
+++ bot/cogs/moderation_cog/infractions_cog.py
@@ -30,7 +30,7 @@ class InfractionsCog(commands.Cog):
     )
     @ext.short_help('Lists a users infractions')
     @ext.example(('infractions', 'infractions @SomeUser'))
-    @ext.required_claims(Claims.moderation_infraction_view)
+    @ext.required_claims(Claims.moderation_infraction_view, Claims.moderation_warn)
     async def infractions(self, ctx: commands.Context, user: t.Optional[discord.Member] = None):
         user = user or ctx.author
         repo = ModerationRepository()
