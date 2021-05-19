689a42597c4766a45813c480be7ac465478205f8
smathep <patricksmathers@icloud.com>
2021-05-19 08:53:20 -0400

    fixed global bans query

--- bot/data/moderation_repository.py
+++ bot/data/moderation_repository.py
@@ -151,7 +151,8 @@ class ModerationRepository(BaseRepository):
         async with aiosqlite.connect(self.resolved_db_path) as db:
             async with db.execute(f"""
                                     SELECT * FROM Infractions
-                                    WHERE fk_subjectId = ?
+                                    WHERE fk_subjectId = ? AND
+                                    iType = '{Infractions.ban}'
                                     """,
                                     (member_id,)) as c:
                 return await self.fetcthall_as_class(c)
