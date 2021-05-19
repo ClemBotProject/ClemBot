66842da65574bfe8a662e00435c5db3752e40e4b
Patrick Smathers <71954677+smathep@users.noreply.github.com>
2021-05-18 21:08:00 -0400

    Added more to the info cog (#343)

--- bot/cogs/info_cog.py
+++ bot/cogs/info_cog.py
@@ -21,44 +21,50 @@ class InfoCog(commands.Cog):
     @ext.example(('info @user', 'info 123456789', 'info'))
     @ext.required_claims(Claims.moderation_infraction_view)
     @ext.ignore_claims_pre_invoke()
-    async def info(self, ctx, member: discord.Member = None):
-        #If the command is invoked without a specified member, it will return info on the calling user
-        if not member:
-            member = ctx.author
-        log.info(f'User {ctx.author} has ran info command on user {member.name}')
+    async def info(self, ctx, user: discord.User = None):
+        #If the command is invoked without a specified user, it will return info on the calling user
+        if not user:
+            user = ctx.author
+        
+        log.info(f'User {ctx.author} has ran info command on user {user.name}')
 
         embed = discord.Embed(title = f'Guild Member Information', color=Colors.ClemsonOrange)
-        embed.set_author(name=f'{member.name}#{member.discriminator}', icon_url=member.avatar_url)
+        embed.set_author(name=f'{user.name}#{user.discriminator}', icon_url=user.avatar_url)
 
-        user_info = f'» **Nickname:** {member.mention}'
-        user_info += f'\n» **ID:** {member.id}'
-        #Some info doesn't need to be available to anyone to check
-        if await self.bot.claims_check(ctx=ctx):
+        user_info = f'» **Nickname:** {user.mention}'
+        user_info += f'\n» **ID:** {user.id}'
+        #Some info doesn't need to be available to anyone to check on for other users
+        if (await self.bot.claims_check(ctx=ctx)) or (ctx.author.id == user.id):
             #prints the datetime in the format of <Months> <day> <year> <hours>:<minutes>:<seconds> <AM/PM>
-            user_info += '\n» **Created:** ' + member.created_at.strftime('%b %d %Y %I:%M:%S %p')
-        embed.add_field(name='**Member ID:**', value=user_info)
-        embed.set_thumbnail(url=member.avatar_url)
-
+            user_info += '\n» **Created:** ' + user.created_at.strftime('%b %d %Y %I:%M:%S %p')
+        embed.add_field(name='**User ID:**', value=user_info)
+        embed.set_thumbnail(url=user.avatar_url)
+            
         #We don't want moderation info available for regular guild users to check on others
-        if await self.bot.claims_check(ctx=ctx) is True:
-            moderation_info = f'» **Warnings:** {len(await ModerationRepository().get_all_warns_member(member.guild.id, member.id))}'
-            moderation_info += f'\n» **Active Mutes:** {len(await ModerationRepository().get_all_active_mutes_member(member.guild.id, member.id))}'
-            moderation_info += f'\n» **Total Infractions:** {len(await ModerationRepository().get_all_infractions_member(member.guild.id, member.id))}'
+        if (await self.bot.claims_check(ctx=ctx)) or (ctx.author.id == user.id):
+            moderation_info = f'» **Warnings:** {len(await ModerationRepository().get_all_warns_member(ctx.guild.id, user.id))}'
+            moderation_info += f'\n» **Active Mutes:** {len(await ModerationRepository().get_all_active_mutes_member(ctx.guild.id, user.id))}'
+            moderation_info += f'\n» **Total Infractions:** {len(await ModerationRepository().get_all_infractions_member(ctx.guild.id, user.id))}'
+            moderation_info += f'\n» **Total Global Bans:** {len(await ModerationRepository().get_global_bans(user.id))}'
             embed.add_field(name='**Moderation Info:**', value=moderation_info)
 
-        guild_info = ''
-        if await self.bot.claims_check(ctx=ctx) is True:
-            #prints the datetime in the format of <Months> <day> <year> <hours>:<minutes>:<seconds> <AM/PM>
-            guild_info = '» **Joined:** ' + member.joined_at.strftime('%b %d %Y %I:%M:%S %p') 
-        guild_info += f'\n» **Message count (last 30 days):** {await MessageRepository().get_user_message_count_range(member.id, member.guild.id, 30)}'
-        guild_info += f'\n» **Roles:** '
-        log.info(f'User has roles: {member.roles}')
-        #skipping the first index because it is just @everyone
-        for i in member.roles[1::]:
-            guild_info += f'{i.mention}'
-        guild_info += f'\n» **Highest role:** {member.top_role.mention}'
-        guild_info += f'\n» **Nitro boost date:** {member.premium_since}'
-        embed.add_field(name='**Guild Information:**', value=guild_info, inline=False)
+        #since member objects include guild ID's and information, we try to get a member object for the target user
+        member = ctx.guild.get_member(user.id)
+        #check to see if the target user is in the calling context's guild. If None then the user isn't in the calling server
+        if member:
+            guild_info = ''
+            if (await self.bot.claims_check(ctx=ctx)) or (ctx.author.id == user.id):
+                #prints the datetime in the format of <Months> <day> <year> <hours>:<minutes>:<seconds> <AM/PM>
+                guild_info = '» **Joined:** ' + member.joined_at.strftime('%b %d %Y %I:%M:%S %p') 
+            guild_info += f'\n» **Message count (last 30 days):** {await MessageRepository().get_user_message_count_range(member.id, ctx.guild.id, 30)}'
+            guild_info += f'\n» **Roles:** '
+            log.info(f'User has roles: {member.roles}')
+            #skipping the first index because it is just @everyone
+            for i in member.roles[1::]:
+                guild_info += f'{i.mention}'
+            guild_info += f'\n» **Highest role:** {member.top_role.mention}'
+            guild_info += '\n» **Nitro boost date:** ' + (member.premium_since.strftime('%b %d %Y %I:%M:%S %p') if member.premium_since is not None else 'Not boosting')
+            embed.add_field(name='**Guild Information:**', value=guild_info, inline=False)
         
         embed.set_footer(text=f'{ctx.author.name}#{ctx.author.discriminator}', icon_url=ctx.author.avatar_url)
         msg = await ctx.send(embed=embed)
