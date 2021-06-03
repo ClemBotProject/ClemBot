79b4bedba4148ca257a3faf4f159a08b1ee56d8d
GlitchedCoder <jlkris007@gmail.com>
2021-06-02 20:02:35 -0400

    Added spaces in-between roles & cleaned up class

--- bot/cogs/info_cog.py
+++ bot/cogs/info_cog.py
@@ -10,6 +10,7 @@ from bot.messaging.events import Events
 
 log = logging.getLogger(__name__)
 
+
 class InfoCog(commands.Cog):
 
     def __init__(self, bot):
@@ -22,7 +23,7 @@ class InfoCog(commands.Cog):
     @ext.required_claims(Claims.moderation_infraction_view)
     @ext.ignore_claims_pre_invoke()
     async def info(self, ctx, user: discord.User = None):
-        #If the command is invoked without a specified user, it will return info on the calling user
+        # If the command is invoked without a specified user, it will return info on the calling user
         if not user:
             user = ctx.author
         
@@ -33,14 +34,14 @@ class InfoCog(commands.Cog):
 
         user_info = f'» **Nickname:** {user.mention}'
         user_info += f'\n» **ID:** {user.id}'
-        #Some info doesn't need to be available to anyone to check on for other users
+        # Some info doesn't need to be available to anyone to check on for other users
         if (await self.bot.claims_check(ctx=ctx)) or (ctx.author.id == user.id):
-            #prints the datetime in the format of <Months> <day> <year> <hours>:<minutes>:<seconds> <AM/PM>
+            # prints the datetime in the format of <Months> <day> <year> <hours>:<minutes>:<seconds> <AM/PM>
             user_info += '\n» **Created:** ' + user.created_at.strftime('%b %d %Y %I:%M:%S %p')
         embed.add_field(name='**User ID:**', value=user_info)
         embed.set_thumbnail(url=user.avatar_url)
             
-        #We don't want moderation info available for regular guild users to check on others
+        # We don't want moderation info available for regular guild users to check on others
         if (await self.bot.claims_check(ctx=ctx)) or (ctx.author.id == user.id):
             moderation_info = f'» **Warnings:** {len(await ModerationRepository().get_all_warns_member(ctx.guild.id, user.id))}'
             moderation_info += f'\n» **Active Mutes:** {len(await ModerationRepository().get_all_active_mutes_member(ctx.guild.id, user.id))}'
@@ -48,20 +49,23 @@ class InfoCog(commands.Cog):
             moderation_info += f'\n» **Total Global Bans:** {len(await ModerationRepository().get_global_bans(user.id))}'
             embed.add_field(name='**Moderation Info:**', value=moderation_info)
 
-        #since member objects include guild ID's and information, we try to get a member object for the target user
+        # since member objects include guild ID's and information, we try to get a member object for the target user
         member = ctx.guild.get_member(user.id)
-        #check to see if the target user is in the calling context's guild. If None then the user isn't in the calling server
+        # check to see if the target user is in the calling context's guild.
+        # If None then the user isn't in the calling server
         if member:
             guild_info = ''
             if (await self.bot.claims_check(ctx=ctx)) or (ctx.author.id == user.id):
-                #prints the datetime in the format of <Months> <day> <year> <hours>:<minutes>:<seconds> <AM/PM>
+                # prints the datetime in the format of <Months> <day> <year> <hours>:<minutes>:<seconds> <AM/PM>
                 guild_info = '» **Joined:** ' + member.joined_at.strftime('%b %d %Y %I:%M:%S %p') 
             guild_info += f'\n» **Message count (last 30 days):** {await MessageRepository().get_user_message_count_range(member.id, ctx.guild.id, 30)}'
             guild_info += f'\n» **Roles:** '
             log.info(f'User has roles: {member.roles}')
-            #skipping the first index because it is just @everyone
+            # skipping the first index because it is just @everyone
+            roles = list[str]()
             for i in member.roles[1::]:
-                guild_info += f'{i.mention}'
+                roles.append(i.mention)
+            guild_info += ' '.join(roles)
             guild_info += f'\n» **Highest role:** {member.top_role.mention}'
             guild_info += '\n» **Nitro boost date:** ' + (member.premium_since.strftime('%b %d %Y %I:%M:%S %p') if member.premium_since is not None else 'Not boosting')
             embed.add_field(name='**Guild Information:**', value=guild_info, inline=False)
@@ -74,4 +78,3 @@ class InfoCog(commands.Cog):
 
 def setup(bot):
     bot.add_cog(InfoCog(bot))
-    
