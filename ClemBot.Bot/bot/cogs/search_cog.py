e1d50aa256b4f117a62e2b484e3c736b51b0e890
GlitchedCoder <jlkris007@gmail.com>
2021-06-13 16:50:12 -0400

    Cleaning up & fixed lint issues.
    * Changed module-level constants to SCREAMING_SNAKE_CASE
    * Changed the categories enum to a dict instead
    * Removed the enum import
    * Removed a currently useless __getitem__ for SearchResult
    * Changed related_topics_formatted to use enumerate in the for-loop.
    * Changed to double-quotes for @ext.long_help
    * Changed to single quotes for the data dict in duck_search
    * Changed a log.info to log.debug so console isn't spammed.
    * Changed category_from_code to fit the dict instead of an enum.
    * Moved category_from_code above the setup method.

--- ClemBot.Bot/bot/cogs/search_cog.py
+++ ClemBot.Bot/bot/cogs/search_cog.py
@@ -5,7 +5,6 @@ import aiohttp
 import discord
 import discord.ext.commands as commands
 import bot.extensions as ext
-from enum import Enum
 from markdownify import markdownify
 from urllib.parse import urlencode
 from urllib.parse import unquote_plus
@@ -13,35 +12,24 @@ from bot.consts import Colors
 from bot.messaging.events import Events
 
 log = logging.getLogger(__name__)
-max_related_topics = 5
-image_url = 'https://duckduckgo.com'
-search_url = 'https://api.duckduckgo.com/'
-icon_url = 'https://i.imgur.com/Knq2kVa.png'
-related_topics_pattern = re.compile('(https://duckduckgo.com/)([acdne]/)?(\\S+)', re.IGNORECASE)
-
-
-class Category(Enum):
-    ARTICLE = 'A', 'Article'
-    DISAMBIGUATION = 'D', 'Disambiguation'
-    CATEGORY = 'C', 'Category'
-    NAME = 'N', 'Name'
-    EXCLUSIVE = 'E', 'Exclusive'
-    NONE = 'None', 'None'
-
-    def code(self):
-        return self.value[0]
-
-    def name(self):
-        return self.value[1]
+MAX_RELATED_TOPICS = 5
+IMAGE_URL = 'https://duckduckgo.com'
+SEARCH_URL = 'https://api.duckduckgo.com/'
+ICON_URL = 'https://i.imgur.com/Knq2kVa.png'
+RELATED_TOPICS_PATTERN = re.compile('(https://duckduckgo.com/)([acdne]/)?(\\S+)', re.IGNORECASE)
+CATEGORIES = {
+    'A': 'Article',
+    'C': 'Category',
+    'D': 'Disambiguation',
+    'N': 'Name',
+    'E': 'Exclusive'
+}
 
 
 class SearchResult:
 
     def __init__(self, json_response):
         self.json_response = json_response
-
-    def __getitem__(self, item):
-        return self.json_response
 
     def has_result(self):
         return len(self.json_response['Heading']) > 0
@@ -61,15 +49,14 @@ class SearchResult:
     def thumbnail(self):
         # surprisingly 'Image' in the JSON returns the latter half of the URL,
         # so we need to concat the other half, i.e., https://duckduckgo.com
-        return image_url + self.json_response['Image']
+        return IMAGE_URL + self.json_response['Image']
 
     def related_topics(self) -> list:
         # related topics can return topics that we don't actually want,
         # so we're taking the subset of data that we can use and returning it
         topics = list()
         related_topics = self.json_response['RelatedTopics']
-        for i in range(0, len(related_topics)):
-            topic = related_topics[i]
+        for topic in related_topics[0::]:
             if 'FirstURL' not in topic:
                 break
             topics.append(topic)
@@ -80,17 +67,19 @@ class SearchResult:
 
     def related_topics_formatted(self):
         related_topics = self.related_topics()
-        related_topics_formatted = list[str]()
+        related_topics_formatted = list()
         # maximum related topics can be changed in the global vars
-        for i in range(0, min(max_related_topics, len(related_topics))):
-            topic = related_topics[i]
-            # stupid way of getting short related topics titles
-            title = related_topics_pattern\
-                .match(topic['FirstURL'])\
+        max_topics = min(MAX_RELATED_TOPICS, len(related_topics))
+        for i, val in enumerate(related_topics):
+            if i >= max_topics:
+                break
+            url = val['FirstURL']
+            title = RELATED_TOPICS_PATTERN\
+                .match(url)\
                 .group(3)\
                 .replace('_', ' ')
             title = unquote_plus(title)
-            related_topics_formatted.append(f"[{title}]({topic['FirstURL']})")
+            related_topics_formatted.append(f'[{title}]({url})')
         return '\n'.join(related_topics_formatted)
 
     def url(self):
@@ -107,7 +96,7 @@ class SearchCog(commands.Cog):
 
     @ext.command()
     @ext.short_help('Searches via DuckDuckGo.')
-    @ext.long_help('Searches via DuckDuckGo\'s instant web API to bring up results from Wikipedia, wikiHow, and more.')
+    @ext.long_help("Searches via DuckDuckGo's instant web API to bring up results from Wikipedia, wikiHow, and more.")
     @ext.example(['search clemson', 'search computer science', 'search how to tie a tie'])
     async def search(self, ctx, *, query: str):
         result = await self.duck_search(query)
@@ -116,25 +105,25 @@ class SearchCog(commands.Cog):
 
     async def duck_search(self, query: str) -> SearchResult:
         data = urlencode({
-            "q": query,
-            "format": "json",
-            "pretty": 1,
-            "skip_disambig": 1,
-            "t": "ClemBot"
+            'q': query,
+            'format': 'json',
+            'pretty': 1,
+            'skip_disambig': 1,
+            't': 'ClemBot'
         })
-        log.debug(f'Search URL: {search_url}?{data}')
+        log.debug(f'Search URL: {SEARCH_URL}?{data}')
         async with aiohttp.ClientSession() as session:
             # redirects should never happen so it's gonna be off by default just in case
-            async with session.get(url=f'{search_url}?{data}', allow_redirects=False) as resp:
+            async with session.get(url=f'{SEARCH_URL}?{data}', allow_redirects=False) as resp:
                 response = json.loads(await resp.text())
-            log.info(response)
+        log.debug(response)
         return SearchResult(response)
 
     async def results(self, ctx, result: SearchResult) -> discord.Message:
-        embed = discord.Embed(title=f'[{result.category().name()}] {result.title()} - {result.source()}',
+        embed = discord.Embed(title=f'[{result.category()}] {result.title()} - {result.source()}',
                               color=Colors.ClemsonOrange,
                               description=f'{result.abstract()}\n\n**Link**: [{result.title()}]({result.url()})')
-        embed.set_footer(text='Result provided by DuckDuckGo.', icon_url=icon_url)
+        embed.set_footer(text='Result provided by DuckDuckGo.', icon_url=ICON_URL)
         if result.has_related_topics():
             embed.add_field(name='Related Topics', value=result.related_topics_formatted(), inline=False)
         if result.has_thumbnail():
@@ -144,16 +133,17 @@ class SearchCog(commands.Cog):
     async def no_results(self, ctx, query: str) -> discord.Message:
         embed = discord.Embed(title='No Results', color=Colors.Error,
                               description=f"Your query '{query}' yielded 0 results.")
-        embed.set_footer(text='Result provided by DuckDuckGo.', icon_url=icon_url)
+        embed.set_footer(text='Result provided by DuckDuckGo.', icon_url=ICON_URL)
         return await ctx.send(embed=embed)
+
+
+def category_from_code(code: str):
+    # going to capitalize it just in case
+    code = code.upper()
+    if code not in CATEGORIES:
+        return 'None'
+    return CATEGORIES[code]
 
 
 def setup(bot):
     bot.add_cog(SearchCog(bot))
-
-
-def category_from_code(code):
-    for c in Category:
-        if code == c.code():
-            return c
-    return Category.NONE
