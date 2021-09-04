75631ed5434ff87d3f54514d16dbb6c5a736fe7c
Jay Madden <jaymaddencox@gmail.com>
2021-09-03 21:15:06 -0400

    Moved manual message content compliance job to quartz.net job (#414)

---
+++ ClemBot.Api/ClemBot.Api.Services/Jobs/MessageContentDeletionJob.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ClemBot.Api.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace ClemBot.Api.Services.Jobs
+{
+    public class MessageContentDeletionJob : IJob
+    {
+        private readonly ClemBotContext _context;
+        private readonly ILogger<MessageContentDeletionJob> _logger;
+
+        private const int MESSAGE_RETENTION_TIME = 30;
+
+        public MessageContentDeletionJob(ClemBotContext context, ILogger<MessageContentDeletionJob> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Job method that defines the compliance task to be executed on a Quartz.Net timer
+        /// </summary>
+        /// <param name="context"></param>
+        public async Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Beginning Discord compliance Message Content Deletion");
+
+            var result = await _context.Database.ExecuteSqlRawAsync(@$"
+                UPDATE ""MessageContents""
+                SET ""Content"" = 'Content Deleted'
+                WHERE ""MessageContents"".""Time""::date <= (NOW()::date -{MESSAGE_RETENTION_TIME}) at time zone 'utc'
+                ");
+
+            _logger.LogInformation($"Discord compliance Message Content Deletion complete: {result} row(s) effected");
+        }
+    }
+}
