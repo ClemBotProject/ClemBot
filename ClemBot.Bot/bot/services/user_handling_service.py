521da5ca4cc2ad534c00bcf20fb81537507dda9d
Jay Madden <jaymaddencox@gmail.com>
2022-01-25 15:52:49 -0600

    Fix seq log statements in prod user_handling_service.py (#578)

--- ClemBot.Bot/bot/services/user_handling_service.py
+++ ClemBot.Bot/bot/services/user_handling_service.py
@@ -93,19 +93,19 @@ class UserHandlingService(BaseService):
     async def add_to_queue(self, guild: discord.Guild, user: discord.Member, role_ids: t.List[int]):
 
         # Check if the guild_id is the in the queue dict, if it's not we need to create the queue first
-        log.info('starting add_to_queue')
         if guild.id not in self.user_update_queue:
             log.info('Creating user_update queue for guild {guild}', guild=serializers.log_guild(guild))
             self.user_update_queue[guild.id] = asyncio.Queue()
 
             # Create the polling task to loop over the queue and dispatch events
-            log.info('starting queue create task')
             asyncio.create_task(self.send_guild_queue(guild.id))
 
         event = UpdateEvent(user.id, role_ids)
-        log.info('starting queue get qsize')
         size = self.user_update_queue[guild.id].qsize()
-        log.info('Adding UserUpdate {event} to {queue} with new size: {size}', event=event, queue=guild.id, size=size+1)
+        log.info('Adding UserUpdate {event} to {queue} with new size: {size}',
+                 event=dataclasses.asdict(event),
+                 queue=guild.id,
+                 size=size+1)
         await self.user_update_queue[guild.id].put(event)
 
     async def send_guild_queue(self, guild_id: int):
@@ -124,9 +124,8 @@ class UserHandlingService(BaseService):
                 # del self.user_update_queue[guild_id]
                 # return
 
-            log.warning('test dispatch')
             log.info('Dispatching update event: {event} on queue: {queue} new queue size: {size}',
-                     event=event,
+                     event=dataclasses.asdict(event),
                      queue=guild_id,
                      size=size)
             await self.bot.user_route.update_roles(event.user_id, event.user_roles_ids, raise_on_error=False)
