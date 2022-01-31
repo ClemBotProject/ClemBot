b41f2f270023d5938c49cfb08fe87cf9b1c98fc7
Jay Madden <jaymaddencox@gmail.com>
2022-01-30 18:27:04 -0600

    initial ClemBot.Docs commit

---
+++ ClemBot.Docs/docs/intro.md
@@ -0,0 +1,24 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Introduction
+
+Hello there and welcome to **ClemBot**! ClemBot is a modular and configurable Discord bot for all of your server needs. For a complete list of all ClemBot commands run the !help command in a server that ClemBot is in.
+
+## Getting Started
+
+After ClemBot has been added to your server and initialized, it's time to do some basic setup. The first thing to do is decide which command prefix you would like ClemBot to respond to. By default ClemBot will respond to "!" as well as ClemBot's mention. To change this, simply run the prefix command with the new prefix you would like ClemBot to respond to. If you are fine with "!" remaining the prefix, you may skip this step.
+
+
+
+## Example
+```
+!prefix ?
+```
+or
+```
+@ClemBot prefix ?
+```
+Congratulations, you've set up ClemBot to respond in your server!
---
+++ ClemBot.Docs/docusaurus.config.js
@@ -0,0 +1,81 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "ClemBot",
+  tagline: "Dinosaurs are cool",
+  url: "https://clembot.netlify.app",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/ClemBotLogo.svg",
+  organizationName: "ClemBotProject", // Usually your GitHub org/user name.
+  projectName: "ClemBot", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/", // Serve the docs at the site's root
+          /* other docs plugin options */
+        },
+        blog: false, // Optional: disable the blog plugin
+        theme: {
+          customCss: [require.resolve("./src/css/custom.css")],
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "ClemBot Wiki",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/ClemBotLogo.svg",
+        },
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discord.gg/mhrVuyh",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/ClemBotProject/ClemBot",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} ClemBot, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
