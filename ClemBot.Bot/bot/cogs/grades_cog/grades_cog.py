ead37201cb6560d251da28f8125633fd4141d609
Makayla Moster <monkmonster96@gmail.com>
2022-03-19 14:16:55 -0400

    fixed footer embed bug in list_prof()

--- ClemBot.Bot/bot/cogs/grades_cog/grades_cog.py
+++ ClemBot.Bot/bot/cogs/grades_cog/grades_cog.py
@@ -32,7 +32,7 @@ class GradesCog(commands.Cog):
 
         self.grades_df.info()
 
-        self.all_profs = self.get_profs()
+        # self.all_profs = self.get_profs() get_profs() not used anymore - put within list_profs()
         self.all_courses = self.get_courses()
 
     @ext.group(invoke_without_command=True, case_insensitive=True)
@@ -292,7 +292,45 @@ class GradesCog(commands.Cog):
                                          channel=ctx.channel,
                                          timeout=360)
 
-    def get_profs(self):
+    # get_profs() was only used once, within the list_prof() function and was creating a duplicate footers bug - for every call, the footer was duplicated indefinetly 
+    # def get_profs(self):
+    #     profs = self.grades_df.groupby(['Instructor']).mean().iterrows()
+
+    #     embeds = []
+
+    #     # begin generating paginated columns
+    #     # chunk the list of tags into groups of TAG_CHUNK_SIZE for each page
+    #     for chunk in self.chunk_list([prof.name for i, prof in profs], TAG_CHUNK_SIZE):
+
+    #         # we need to create the columns on the page so chunk the list again
+    #         content = ''
+    #         for col in self.chunk_list(chunk, 2):
+    #             #the columns wont have the perfect number of elements every time, we need to append spaces if
+    #             #the list entries is less then the number of columns
+    #             while len(col) < 3:
+    #                 col.append(' ')
+
+    #             # Cocatenate the formatted column string to the page content string
+    #             content += "{: <24}  {: <24}\n".format(*col)
+
+    #         #Append the content string to the list of pages to send to the paginator
+    #         #Marked as a code block to ensure a monospaced font and even columns
+    #         embed = discord.Embed(title='All Known Professors', color=Colors.ClemsonOrange)
+    #         embed.add_field(name='Listings:', value=f'```{content}```')
+    #         embed.add_field(name='Info:',
+    #                         value=f'Clemson provides incomplete and mangled data so there may be multiple different versions of the same professor as well as other mangled names. This is just a byproduct of how the data is distributed by the university',
+    #                         inline=False)
+
+    #         embeds.append(embed)
+    #     return embeds
+
+    @prof.command(aliases=['list'])
+    @ext.long_help(
+        'Lists all available professors, some professors will just be a name with no data'
+    )
+    @ext.short_help('Lists all professors')
+    @ext.example('prof list')
+    async def list_prof(self, ctx):
         profs = self.grades_df.groupby(['Instructor']).mean().iterrows()
 
         embeds = []
@@ -321,17 +359,9 @@ class GradesCog(commands.Cog):
                             inline=False)
 
             embeds.append(embed)
-        return embeds
 
-    @prof.command(aliases=['list'])
-    @ext.long_help(
-        'Lists all available professors, some professors will just be a name with no data'
-    )
-    @ext.short_help('Lists all professors')
-    @ext.example('prof list')
-    async def list_prof(self, ctx):
         await self.bot.messenger.publish(Events.on_set_pageable_embed,
-                                         pages=self.all_profs,
+                                         pages=embeds,
                                          author=ctx.author,
                                          channel=ctx.channel,
                                          timeout=360)
