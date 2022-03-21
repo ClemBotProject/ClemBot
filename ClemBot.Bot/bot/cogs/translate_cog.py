c832ed5c620098af703ebee007cf8ae4f556ebcf
Stephen Martin <93275421+ausernameforgithub1654@users.noreply.github.com>
2022-03-21 00:37:48 -0400

    final fixes

--- ClemBot.Bot/bot/cogs/translate_cog.py
+++ ClemBot.Bot/bot/cogs/translate_cog.py
@@ -14,7 +14,7 @@ from bot.messaging.events import Events
 
 log = logging.getLogger(__name__)
 
-TRACE_ID = str(uuid.uuid4())
+
 
 
 class TranslateCog(commands.Cog):
@@ -24,38 +24,22 @@ class TranslateCog(commands.Cog):
 
     @ext.group(case_insensitive=True, invoke_without_command=True)
     @ext.long_help(
-        'Allows you to translate words or sentences by specifying an output language and autodetecting the language you entered. Use !translate m for specifying the language you entered. Use \'translate languages\' to see available languages')
+        'Allows you to translate words or sentences by either specifying both the input and output language with the text to translate, or just the output language and the text to translate. run \'translate languages\' to see available languages')
     @ext.short_help('Translates words or phrases between two languages')
-    @ext.example(('translate es Hello', 'translate german Hello', 'translate languages', 'translate German Como estas?, translate m german english I love partying with german people!'))
+    @ext.example(('translate en spanish Hello', 'translate german Hello', 'translate languages', 'translate Spanish German Como estas?'))
     async def translate(self, ctx, *input: str):
         if len(input) < 2:
             raise UserInputError("Incorrect Number of Arguments. Minimum of 2 arguments")
 
-        if (is_valid_lang_code(input[0]) != None):
-          await self.translate_given_lang(ctx, input)
+        if get_lang_code(input[0]):
+            await self.translate_given_lang(ctx, input)
         else:
-            raise UserInputError("Invalid input! Use !translate m for manual translation.")
+            raise UserInputError("Incorrect country code or syntax. Use the help command")
 
-    @translate.command(aliases=["manual", "man"])
-    @ext.long_help('Alternative command to manually specify languages to convert to.')
-    @ext.short_help('!translate m <output lang> <input lang> <text>')
-    @ext.example(('!translate m english spanish Me encantan las fiestas con amigos'))
-    async def m(self, ctx, *input: str):
-        if len(input) < 3:
-            raise UserInputError("Incorrect Number of Arguments. Minimum number of 3 arguments")
-        if (is_valid_lang_code(input[0]) and is_valid_lang_code(input[1])) != None:
-            await self.alternative_translate_lang(ctx, input)
-        else:
-              raise UserInputError("Invalid input! Specify the language you entered.")
-
-        return
-
-          
     @translate.command()
     @ext.long_help('Shows all available languages to translate between')
     @ext.short_help('Shows available languages')
     @ext.example(('translate languages'))
-
     async def languages(self, ctx):
         await self.bot.messenger.publish(Events.on_set_pageable_text,
                                          embed_name='Languages',
@@ -64,13 +48,47 @@ class TranslateCog(commands.Cog):
                                          author=ctx.author,
                                          channel=ctx.channel)
         return
-        
+    @translate.command(aliases=['m'])
+    @ext.long_help('Manually specifies an output language to translate too')
+    @ext.short_help('Specify an output language')
+    @ext.example('translate m or translate manual')
+    async def manual(self, ctx, input):
+        output_lang = await get_lang_code(self, ctx, input[0])
+        input_lang = await get_lang_code(self,ctx,input[1])
+        if not (input_lang and output_lang):
+            return
+        text = ' '.join(input[2:])
+        params = {
+            'api-version': '3.0',
+            'from': input_lang,
+            'to': output_lang
+        }
 
-    
+        body = [{
+            'text': text
+        }]
 
-async def translate_given_lang(self, ctx, input):
-        output_lang = is_valid_lang_code(input[0])
+        headers = {
+            'Ocp-Apim-Subscription-Key': bot_secrets.secrets.azure_translate_key,
+            'Ocp-Apim-Subscription-Region': 'global',
+            'Content-type': 'application/json',
+            'X-ClientTraceId': TRACE_ID
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with await session.post(url=TRANSLATE_API_URL, params=params, headers=headers, json=body) as resp:
+                response = json.loads(await resp.text())
+
+        embed = discord.Embed(title='Translate', color=Colors.ClemsonOrange)
+        name = 'Translated to ' + LANGUAGE_SHORT_CODE_TO_NAME[response[0]['translations'][0]['to']]
+        embed.add_field(name=name, value=response[0]['translations'][0]['text'], inline=False)
+        await ctx.send(embed=embed)
+
+
+    async def translate_given_lang(self, ctx, input):
+        output_lang = await get_lang_code(self, ctx, input[0])
         text = ' '.join(input[1:])
+
         params = {
             'api-version': '3.0',
             'to': output_lang
@@ -91,90 +109,41 @@ async def translate_given_lang(self, ctx, input):
             async with await session.post(url=TRANSLATE_API_URL, params=params, headers=headers, json=body) as resp:
                 response = json.loads(await resp.text())
         embed = discord.Embed(title='Translate', color=Colors.ClemsonOrange)
-        name = 'Translated to ' + LANGUAGE_SHORT_CODE_TO_NAME[response[0]['translations'][0]['to'].lower()]
+        name = 'Translated to ' + LANGUAGE_SHORT_CODE_TO_NAME[response[0]['detectedLanguage']['language']]
+        embed.add_field(name='Confidence Level:', value=response[0]['detectedLanguage']['score'], inline=True)
         embed.add_field(name=name, value=response[0]['translations'][0]['text'], inline=False)
         await ctx.send(embed=embed)
         return
 
 
 
-def is_valid_lang_code(input: str):
-        if(input in LANGUAGE_NAME_TO_SHORT_CODE):
-            return LANGUAGE_NAME_TO_SHORT_CODE.get(input)
-        elif(input in LANGUAGE_SHORT_CODE_TO_NAME):
-            return input
-        elif(input in LOWERCASE_LANGUAGENAME):
-          b = LOWERCASE_LANGUAGENAME.index(input)
-          good_list = list(LANGUAGE_SHORT_CODE_TO_NAME)
-          return good_list[b]
+async def get_lang_code(self, ctx, language: str):
+    language_lower = language.lower()
 
-        elif(input in LOWERCASE_LANGUAGE_SHORT_CODE_TO_NAME):
-          g = LOWERCASE_LANGUAGE_SHORT_CODE_TO_NAME.index(input)
-          epic_list = list(LANGUAGE_SHORT_CODE_TO_NAME)
-          return epic_list[g] 
+    if language_lower in LOWER_CODE_TO_CODE:
+        return LOWER_CODE_TO_CODE[language_lower]
+    elif language_lower in LOWER_LANGUAGE_TO_CODE:
+        return LOWER_LANGUAGE_TO_CODE[language_lower]
+    else:
+            pages = get_language_list(self)
+            await self.bot.messenger.publish(Events.on_set_pageable_text,
+                                   embed_name='Languages',
+                                   field_title='Given language \'' + language + '\' not valid. Here are the available languages:',
+                                   pages=pages,
+                                   author=ctx.author,
+                                   channel=ctx.channel)
+            return False
 
-        elif(input.lower in LOWERCASE_LANGUAGENAME):
-            p = input.lower
-            g = LOWERCASE_LANGUAGENAME.index(p)
-            awesome_list = list(LANGUAGE_SHORT_CODE_TO_NAME)
-            return awesome_list[g]
-
-        elif(input.lower in LOWERCASE_LANGUAGE_SHORT_CODE_TO_NAME):
-            l = input.lower
-            g = LOWERCASE_LANGUAGE_SHORT_CODE_TO_NAME.index(l)
-            biggest_list = list(LANGUAGE_SHORT_CODE_TO_NAME)
-            return biggest_list[g]
-        else: return None
-  
-    
-    
-async def alternative_translate_lang(self, ctx, input):
-    output_lang = is_valid_lang_code(input[0])
-    input_lang = is_valid_lang_code(input[1])
-    text = ' '.join(input[2:])       
-    params = {
-       'api-version': '3.0',
-       'from': input_lang,
-       'to': output_lang
-        }
-
-        body = [{
-        'text': text
-        }]
-
-        headers = {
-        'Ocp-Apim-Subscription-Key': bot_secrets.secrets.azure_translate_key,
-        'Ocp-Apim-Subscription-Region': 'global',
-        'Content-type': 'application/json',
-        'X-ClientTraceId': TRACE_ID
-         }
-
-        async with aiohttp.ClientSession() as session:
-            async with await session.post(url=TRANSLATE_API_URL, params=params, headers=headers, json=body) as resp:
-                response = json.loads(await resp.text())
-
-    log.info(response[0]['translations'])
-    embed = discord.Embed(title='Translate', color=Colors.ClemsonOrange)
-    name = 'Translated to ' + LANGUAGE_SHORT_CODE_TO_NAME[response[0]['translations'][0]['to'].lower()]
-    embed.add_field(name=name, value=response[0]['translations'][0]['text'], inline=False)
-    await ctx.send(embed=embed)
-    return
-
-def is_valid_lang_code(code: str):
-    code_lower = code.lower()
-    if code_lower in LOWER_CODE_TO_CODE:
-        return LOWER_CODE_TO_CODE[code_lower]
-    if code_lower in LOWER_LANGUAGE_TO_CODE:
-        return LOWER_LANGUAGE_TO_CODE[code_lower]
 
 def get_language_list(self):
     langs = [f'{name} ({short})' for name, short in LANGUAGE_NAME_TO_SHORT_CODE.items()]
     return ['\n'.join(i) for i in chunk_list(self, langs, CHUNK_SIZE)]
 
-    
+
 def chunk_list(self, lst, n):
-      for i in range(0, len(lst), n):
+    for i in range(0, len(lst), n):
         yield lst[i:i + n]
+        
 
 LANGUAGE_NAME_TO_SHORT_CODE = {
 "Afrikaans":"af",      
@@ -285,11 +254,14 @@ LANGUAGE_NAME_TO_SHORT_CODE = {
 "Yucatec Maya":"yua",                           
                          
 }
-
-LOWER_LANGUAGE_TO_CODE = {k.lower(): v for k, v in LANGUAGE_NAME_TO_SHORT_CODE.items()}
-LOWER_CODE_TO_CODE = {k.lower(): k for k, _ in LANGUAGE_NAME_TO_SHORT_CODE.items()}
 CHUNK_SIZE = 15
+LOWER_LANGUAGE_TO_CODE = {k: v for k, v in LANGUAGE_NAME_TO_SHORT_CODE.items()}
+LOWER_CODE_TO_CODE = {k: k for k, _ in LANGUAGE_NAME_TO_SHORT_CODE.items()}
+LANGUAGE_SHORT_CODE_TO_NAME = {value: key for key, value in LANGUAGE_NAME_TO_SHORT_CODE.items()}
 TRANSLATE_API_URL = "https://api.cognitive.microsofttranslator.com/translate"
+
+TRACE_ID = str(uuid.uuid4())
+
 
 def setup(bot):
     bot.add_cog(TranslateCog(bot))
