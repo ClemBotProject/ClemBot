a4a3e1d1de8bb63c5d5f9dca4087feb437024c01
Stephen Martin <93275421+STEVE4git@users.noreply.github.com>
2022-05-07 21:45:54 -0400

    Add files via upload

--- ClemBot.Bot/bot/cogs/trivia_cog.py
+++ ClemBot.Bot/bot/cogs/trivia_cog.py
@@ -1,6 +1,8 @@
+import chunk
 import json
 import logging
 import threading
+from typing import Final
 from urllib import response
 import uuid
 import random
@@ -33,14 +35,14 @@ class TriviaCog(commands.Cog):
     "Use !trivia to return a random assortment of 10 trivia questions. React with emojis to submit your answer choice"    )
     @ext.short_help(
     "!trivia use !trivia m for custom arguments"    )
-    async def Trivia(self, ctx):
+    async def trivia(self, ctx):
         async with await self.session.get(DEFAULT_URL) as resp:
                 theresponse = json.loads(await resp.text())
                 correct_answers = await self.Json_Parser(ctx, theresponse)
              
         dictreturn = await self.Dict_Publisher(ctx, theresponse, correct_answers)                  
                        
-    @Trivia.command(aliases=['m'])
+    @trivia.command(aliases=['m'])
     @ext.long_help(
         "Specify arguments you want to return such as question number (max 35), category, difficulty, or question type. Use numbers for quicker specification of category by typing in the number beside the category in !help. Use 0 for unused categories!")
     @ext.short_help("!trivia m <Question Number> <category/substring/number> <Difficulty/Number> <question type>")
@@ -55,11 +57,9 @@ class TriviaCog(commands.Cog):
      while x < inputlength:
         
         appendthis = await self.Matching_Function(x, *input)
-        print(appendthis)
         FunctionParameters.append(appendthis)
         x+=1    
      url = await self.Url_Builder(FunctionParameters, inputlength)
-     print(url)
      async with await self.session.get(url) as resp:  
             response = json.loads(await resp.text())
             correct_answers = await self.Json_Parser(ctx, response)
@@ -71,6 +71,53 @@ class TriviaCog(commands.Cog):
      theembed = await self.Dict_Publisher(ctx, response, correct_answers)       
 
      return
+    @trivia.command(aliases=['help'])
+    @ext.long_help(
+                    "Lists the categories, difficulty, and type of questions. Useful for finding the index of categories!")
+    @ext.short_help(
+                "Use this to find the category you want!" )     
+    @ext.example(
+            "!trivia help" )                        
+    async def List_Help(self, ctx):
+        embed_list = []
+        Final_Page = []
+        Category_Generator = helper_fixer(CATEGORYLIST)
+        for x in Category_Generator:
+            embed_list.append(x)
+        sizeofcategory = len(embed_list)
+        x = 0
+        while x < sizeofcategory:
+            Category_embed = discord.Embed(title="Category List:", color = Colors.ClemsonOrange)
+            Category_embed.add_field(name="Index:",value=embed_list[x])
+            Final_Page.append(Category_embed)
+            x+=1
+        Difficulty_Generator = helper_fixer(DIFFICULTY)    
+        for i in Difficulty_Generator:
+            embed_list.append(i)
+        new_insertion = len(embed_list)
+        while sizeofcategory < new_insertion:
+            Difficulty_embed = discord.Embed(title="Difficulty List:", color = Colors.ClemsonOrange)
+            Difficulty_embed.add_field(name="Index:",value=embed_list[sizeofcategory])
+            Final_Page.append(Difficulty_embed)
+            sizeofcategory+=1
+        Question_Generator = helper_fixer(QUESTIONTYPE)
+        for i in Question_Generator:
+            embed_list.append(i)
+        last_size = len(embed_list)
+        while new_insertion < last_size:
+            Type_embed = discord.Embed(title="Question Type:", color = Colors.ClemsonOrange)
+            Type_embed.add_field(name="Index:",value=embed_list[new_insertion])
+            Final_Page.append(Type_embed)
+            new_insertion+=1 
+        await self.bot.messenger.publish(Events.on_set_pageable_embed,
+                                         pages=Final_Page,
+                                         author=ctx.author,
+                                         channel=ctx.channel,
+                                         timeout=360,
+                                        )
+       
+         
+        return 
     async def Url_Builder(self, functionlist, inputlength):
             max_index = inputlength-1
             url = URL_BUILDER+ str(functionlist[0])
@@ -86,9 +133,7 @@ class TriviaCog(commands.Cog):
                                  url+=("&type="+functionlist[max_index])
                  x+=1                
             return url   
-                          
-          
-            
+                            
     async def Matching_Function(self, case, *input:str):
      
      match case:
@@ -150,17 +195,7 @@ class TriviaCog(commands.Cog):
                  else:
                         raise UserInputError("Question type invalid.")    
      return                      
-    async def List_Help(self):
-        embed_list = []
-        embed_list.append(self.helper_fixer(CATEGORYLIST))
-        embed_list.append(self.helper_fixer(DIFFICULTY))
-        embed_list.append(self.helper_fixer(QUESTIONTYPE))
-        embed = discord.Embed(title= "Help Page:",
-         descripton="Help Page:",
-         color=Colors.ClemsonOrange)
-        embed.addfield(name="Categories:", value=CATEGORYLIST)
-         
-        return
+  
     async def Json_Parser(self,ctx, dictionary):
         Correct_Answers= []
         for x in dictionary['results']:
@@ -179,7 +214,7 @@ class TriviaCog(commands.Cog):
              random.shuffle(Answers_List)
              List_Index.append(Answers_List.index(Right_Answer))  
              x+=1   
-             embed = discord.Embed(title= "Your Trivia Question #"+str(x)+":",
+             embed = discord.Embed(title= "Question #"+str(x)+":",
              color=Colors.ClemsonOrange)
              embed.add_field(name="Question:", value=bob['question'])
              embed.add_field(name="Category:", value=bob['category'])
@@ -201,27 +236,6 @@ class TriviaCog(commands.Cog):
                                          timeout=360,
                                         )
                                         
-        tester = discord.ext.commands.Paginator.pages                           
-       
-                                      
-                                                                       
-        print("yes\n\n\n\n\n")
-        def newcheck(bot):
-            return bot.author == self.bot.user.id
-        msg = await self.bot.wait_for("message", check=newcheck)                                                                   
-       # await msgembed.add_reaction('🇦')
-       # await msgembed.add_reaction('🇧')
-       # await msgembed.add_reaction('🇨')
-        #await msgembed.add_reaction('🇩')
-       
-        def check(reaction, user):
-            return user == ctx.author and ANSWER_KEY.find(str(reaction.emoji)) != -1
-            
-            
-        while Publish_Embed.is_alive():
-            
-                reaction, user  = await self.client.wait_for("reaction_add", timeout=360, check=check)
-                current_pagenumber = discord.ext.pages.Paginator.current_page
         
         return                                                                          
         
@@ -236,14 +250,15 @@ class TriviaCog(commands.Cog):
             return
    
 
-    def helper_fixer(formatthis):
-         newlist = []
-         for x in formatthis:   #Fix this+ formatting
-           newlist[x] = '\n'.append(formatthis[x]) + "     Index Of Element:"+formatthis.index([x])
-         for i in range(0, len(formatthis), CHUNK_SIZE):
-             yield newlist[i:i+CHUNK_SIZE]          
+def helper_fixer(formatthis):
 
-CHUNK_SIZE = 15
+         new_list = [f'#{formatthis.index(x)+1}.    {x}' for x in formatthis]
+         return ['\n'.join(i) for i in chunk_list(new_list, CHUNK_SIZE)]  #Decided to implement chunking. Although useless now if the category lists expands alot it will be an easy fix.
+def chunk_list(lst, n):
+ for i in range(0, len(lst), n):   
+    yield lst[i:i+n]       
+
+CHUNK_SIZE = 24
 
 ANSWER_KEY=['🇦',
             '🇧',
