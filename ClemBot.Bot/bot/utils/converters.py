fa29cb6c20e45f351a2d29da34e73bd55c56136e
Milo Weinberg <iapetus011@gmail.com>
2022-06-26 21:52:59 -0400

    undo more commenting of match statements (oops)

--- ClemBot.Bot/bot/utils/converters.py
+++ ClemBot.Bot/bot/utils/converters.py
@@ -100,92 +100,91 @@ def trivia_cog_converter(input_length, input_list): #converts the args as fast a
         url_parameters = []
 
         for i in range (0, input_length):
-            pass
-            # match i:  #Revolves around beautiful O(1) based indexing
-            #     case 0: # question number
-            #         if input_list[0].isnumeric():
-            #             question_number = int(input_list[0])
-            #             if 0 < question_number <= 50:
-            #                  url_parameters.append(question_number)
-            #             else:
-            #                 raise UserInputError(
-            #                     "Question Number entered is out of range!")
-            #         else:
-            #             raise UserInputError(
-            #                 "Question Number has to be a number within the range of 1 to 50")
-            #     case 1: #category
-            #         if input_list[1].isnumeric():
-            #             trivia_number = int(input_list[1])
-            #             if 0 < trivia_number <= 24:
-            #                 url_parameters.append(trivia_number + 8)
-            #             elif trivia_number == 0: # special number to allow entry of empty arguments since it would be impossible otherwise
-            #                 url_parameters.append(None)
-            #             else:
-            #                 raise UserInputError(
-            #                     "Category Number out of bounds(Number has to be 1-24) or enter the category you want! Type ?trivia help to see the category list")
-            #         else:
-            #             trivia_category = input_list[1].lower()
+            match i:  #Revolves around beautiful O(1) based indexing
+                case 0: # question number
+                    if input_list[0].isnumeric():
+                        question_number = int(input_list[0])
+                        if 0 < question_number <= 50:
+                             url_parameters.append(question_number)
+                        else:
+                            raise UserInputError(
+                                "Question Number entered is out of range!")
+                    else:
+                        raise UserInputError(
+                            "Question Number has to be a number within the range of 1 to 50")
+                case 1: #category
+                    if input_list[1].isnumeric():
+                        trivia_number = int(input_list[1])
+                        if 0 < trivia_number <= 24:
+                            url_parameters.append(trivia_number + 8)
+                        elif trivia_number == 0: # special number to allow entry of empty arguments since it would be impossible otherwise
+                            url_parameters.append(None)
+                        else:
+                            raise UserInputError(
+                                "Category Number out of bounds(Number has to be 1-24) or enter the category you want! Type ?trivia help to see the category list")
+                    else:
+                        trivia_category = input_list[1].lower()
 
-            #             for x in CATEGORYLIST_LOWER:
-            #                 if x.find(trivia_category) != -1:
-            #                     return_this = CATEGORYLIST_LOWER.index(x) + 9
-            #                     url_parameters.append(return_this)
-            #                     break;
-            #             else:
-            #                 raise UserInputError("Category not found!")
-            #     case 2: #difficulty
-            #         if input_list[2].isnumeric():
-            #             evaluate_int = int(input_list[2])
-            #             if 0 < evaluate_int <= 3:
-            #                 return_string = DIFFICULTY_LOWER[evaluate_int - 1]
-            #                 url_parameters.append(return_string)
-            #             elif evaluate_int == 0:
-            #                 url_parameters.append(None)
-            #             else:
-            #                 raise UserInputError(
-            #                     "Difficulty Number out of bounds(Number has to be 1-3) or enter Easy-Hard! Type ?trivia help to see the difficulty list.")
-            #         else:
-            #             difficulty = input_list[2].lower()
-            #             for x in DIFFICULTY_LOWER:
+                        for x in CATEGORYLIST_LOWER:
+                            if x.find(trivia_category) != -1:
+                                return_this = CATEGORYLIST_LOWER.index(x) + 9
+                                url_parameters.append(return_this)
+                                break;
+                        else:
+                            raise UserInputError("Category not found!")
+                case 2: #difficulty
+                    if input_list[2].isnumeric():
+                        evaluate_int = int(input_list[2])
+                        if 0 < evaluate_int <= 3:
+                            return_string = DIFFICULTY_LOWER[evaluate_int - 1]
+                            url_parameters.append(return_string)
+                        elif evaluate_int == 0:
+                            url_parameters.append(None)
+                        else:
+                            raise UserInputError(
+                                "Difficulty Number out of bounds(Number has to be 1-3) or enter Easy-Hard! Type ?trivia help to see the difficulty list.")
+                    else:
+                        difficulty = input_list[2].lower()
+                        for x in DIFFICULTY_LOWER:
 
-            #                 if (x.find(difficulty) != -1):  #Searches the substring. If this ever comes up, its not a bug you can type in a and not find your category GIGO. It is better than exact case parsing.
-            #                     url_parameters.append(x)
-        #                         break;
-        #                 else:
-        #                     raise UserInputError("Difficulty not found")
-        #         case 3: #question type
-        #             if input_list[3].isnumeric():
-        #                 evaluate_int = int(input_list[3])
-        #                 if 0 < evaluate_int < 3:
-        #                     final_return = QUESTIONTYPE[evaluate_int - 1]
-        #                     url_parameters.append(final_return)
-        #                 elif evaluate_int == 0:
-        #                     url_parameters.append(None)
-        #                 else:
-        #                     raise UserInputError(
-        #                         "Question type number out of bounds(1 or 2) 1: Multiple Choice 2: Boolean. Type ?trivia help to see our question types.")
-        #             else:
-        #                 question_type = input_list[3].lower()
-        #                 for x in QUESTIONTYPE:
-        #                     if (x.find(question_type) != -1):
-        #                        url_parameters.append(x)
-        #                        break;
-        #                 else:
-        #                     raise UserInputError(
-        #                         "Couldn't find the question type you are looking for!.")
+                            if (x.find(difficulty) != -1):  #Searches the substring. If this ever comes up, its not a bug you can type in a and not find your category GIGO. It is better than exact case parsing.
+                                url_parameters.append(x)
+                                break;
+                        else:
+                            raise UserInputError("Difficulty not found")
+                case 3: #question type
+                    if input_list[3].isnumeric():
+                        evaluate_int = int(input_list[3])
+                        if 0 < evaluate_int < 3:
+                            final_return = QUESTIONTYPE[evaluate_int - 1]
+                            url_parameters.append(final_return)
+                        elif evaluate_int == 0:
+                            url_parameters.append(None)
+                        else:
+                            raise UserInputError(
+                                "Question type number out of bounds(1 or 2) 1: Multiple Choice 2: Boolean. Type ?trivia help to see our question types.")
+                    else:
+                        question_type = input_list[3].lower()
+                        for x in QUESTIONTYPE:
+                            if (x.find(question_type) != -1):
+                               url_parameters.append(x)
+                               break;
+                        else:
+                            raise UserInputError(
+                                "Couldn't find the question type you are looking for!.")
         
-        # url = URL_BUILDER + str(url_parameters[0]) #This has to exist or else it would have raised an exception and exited
+        url = URL_BUILDER + str(url_parameters[0]) #This has to exist or else it would have raised an exception and exited
 
-        # for x in range (1, input_length):
-        #     if url_parameters[x]:
-        #         match x:
-        #             case 1:
-        #                url= (f"{url}&category={str(url_parameters[1])}")
-        #             case 2:
-        #                 url = (f"{url}&difficulty={url_parameters[2]}")
-        #             case 3:
-        #                 url = (f"{url}&type={url_parameters[input_length-1]}")
-        # return url
+        for x in range (1, input_length):
+            if url_parameters[x]:
+                match x:
+                    case 1:
+                       url= (f"{url}&category={str(url_parameters[1])}")
+                    case 2:
+                        url = (f"{url}&difficulty={url_parameters[2]}")
+                    case 3:
+                        url = (f"{url}&type={url_parameters[input_length-1]}")
+        return url
 
 
 URL_BUILDER = R"https://opentdb.com/api.php?amount="
