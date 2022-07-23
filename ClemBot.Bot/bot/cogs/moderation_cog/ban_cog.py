4180b8ab0384f61b81d497001375afe63c0513d0
Justin <jlkris007@gmail.com>
2022-07-22 23:57:06 -0400

    Replace get_full_name with str(ctx.author)

--- ClemBot.Bot/bot/cogs/moderation_cog/ban_cog.py
+++ ClemBot.Bot/bot/cogs/moderation_cog/ban_cog.py
@@ -31,7 +31,7 @@ class BanCog(commands.Cog):
             embed = discord.Embed(title='Error', color=Colors.Error)
             embed.add_field(name='Reason',
                             value=f'Reason length is greater than max {Moderation.max_reason_length} characters.')
-            embed.set_author(name=self.get_full_name(ctx.author), icon_url=ctx.author.display_avatar.url)
+            embed.set_author(name=str(ctx.author), icon_url=ctx.author.display_avatar.url)
             return await ctx.send(embed=embed)
 
         if ctx.author.roles[-1].position <= subject.roles[-1].position:
