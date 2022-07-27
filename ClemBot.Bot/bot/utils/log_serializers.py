2ab7accff6f3035a1c632759502522c5cbbc9734
Milo Weinberg <iapetus011@gmail.com>
2022-07-27 10:18:02 -0400

    type log_serializers.py

--- ClemBot.Bot/bot/utils/log_serializers.py
+++ ClemBot.Bot/bot/utils/log_serializers.py
@@ -3,11 +3,11 @@ import typing as t
 import discord
 
 
-def log_guild(guild: discord.Guild):
+def log_guild(guild: discord.Guild) -> dict[str, t.Any]:
     return {"id": guild.id, "name": guild.name}
 
 
-def log_user(member: t.Union[discord.Member, discord.User]):
+def log_user(member: (discord.Member | discord.User)) -> dict[str, t.Any]:
     return (
         {"id": member.id, "name": member.name, "guild": log_guild(member.guild)}
         if isinstance(member, discord.Member)
@@ -18,9 +18,9 @@ def log_user(member: t.Union[discord.Member, discord.User]):
     )
 
 
-def log_channel(channel: discord.TextChannel):
+def log_channel(channel: discord.TextChannel) -> dict[str, t.Any]:
     return {"id": channel.id, "name": channel.name, "guild": log_guild(channel.guild)}
 
 
-def log_role(role: discord.Role):
+def log_role(role: discord.Role) -> dict[str, t.Any]:
     return {"id": role.id, "name": role.name, "guild": log_guild(role.guild)}
