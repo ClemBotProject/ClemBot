c63b6590d8c459fa24a413f0569228b6e09fb32f
Jay Madden <jaymaddencox@gmail.com>
2022-09-22 23:42:07 -0500

    Fix groups not being parsed correctly

--- ClemBot.Bot/bot/utils/converters.py
+++ ClemBot.Bot/bot/utils/converters.py
@@ -34,14 +34,20 @@ class CommandConverter(Converter[ext.ClemBotCommand]):
 
         command = ctx.bot.all_commands.get(self.accu[0])
 
+        if not command:
+            raise ConversionError()
+
         for i, name in enumerate(self.accu[1:]):
             # Check if the newly parsed command is a group which means we need to check the next
             # value to see if it's a valid sub-command
-            if isinstance(command, ext.ClemBotGroup):
-                try:
-                    command = command.all_commands[name]
-                except (AttributeError, KeyError):
-                    raise ConversionError()
+
+            if not isinstance(command, ext.ClemBotGroup):
+                raise ConversionError()
+
+            try:
+                command = command.all_commands[name]
+            except (AttributeError, KeyError):
+                raise ConversionError()
 
         # All Clembot commands should be ClemBotCommands by default so this should never fail
         return t.cast(ext.ClemBotCommand, command)
