fd08428b0cd892cc99fd2904805ec3359020335e
Jay Madden <jaymaddencox@gmail.com>
2022-11-13 22:31:10 -0600

    Initial astro rewrite commit

--- ClemBot.Site/middleware/DashboardAuthCheck.ts
+++
@@ -1,9 +0,0 @@
-import { Middleware } from '@nuxt/types'
-
-const DashboardAuthCheck: Middleware = async ({ app: { $auth }, redirect }) => {
-  if (!$auth.loggedIn) {
-      redirect('/')
-  }
-}
-
-export default DashboardAuthCheck
--- ClemBot.Site/pages/status.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <section class="hero is-fullheight-with-navbar">
-    <div class="hero-body">
-      <p class="title">Coming soon!</p>
-    </div>
-  </section>
-</template>
