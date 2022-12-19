4b453ea4768cfb55293b67c3d744b409f384f6bb
Jay Madden <jaymaddencox@gmail.com>
2022-12-18 20:26:48 -0600

    Add netlify adapater

--- ClemBot.Site/astro.config.mjs
+++ ClemBot.Site/astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({})
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: netlify()
+});
