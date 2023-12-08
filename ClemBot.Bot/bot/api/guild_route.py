c217325096101181b0a1e5c574dfc5f3632449d7
Janet Taylor <janett@clemson.edu>
2023-12-08 11:23:14 -0500

    revert change

--- ClemBot.Bot/bot/api/guild_route.py
+++ ClemBot.Bot/bot/api/guild_route.py
@@ -44,7 +44,7 @@ class GuildRoute(BaseRoute):
 
     async def get_guild_slot_scores(
         self, guild_id: int, limit: int, leader: bool
-    ) -> t.List[SlotScore]:
+    ) -> list[SlotScore]:
         resp = await self._client.get(
             f"bot/Guilds/{guild_id}/SlotScores", params={"leader": str(leader), "limit": limit}
         )
